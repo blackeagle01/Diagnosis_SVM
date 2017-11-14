10f83b6947800ccec8a278e8f5c5dd80bfc38ea3
blackeagle01 <tiku.harsh@gmail.com>
2017-11-14 19:59:36 +0530

    add SVC

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
 import os
 import numpy as np
-
+from sklearn.svm import SVC
 #Data Preprocessing 
 
 
@@ -14,19 +14,47 @@ data=StandardScaler().fit_transform(d.data)
 X_train,X_test,Y_train,Y_test=train_test_split(data,d.target)
 
 
+
+
 #Train a neural network to classify data
 
-model=Sequential()
-model.add(Dense(50,activation='relu',input_dim=30))
-model.add(Dense(50,activation='relu'))
-model.add(Dropout(0.4))
-model.add(Dense(1,activation='sigmoid'))
-model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+
+def SupportVectorMachine():
+	machine=SVC()
+
+	machine.fit(X_train,Y_train)
+
+	return machine
+
+
+
+
+
+
+def neural_net():
+
+	model=Sequential()
+	model.add(Dense(50,activation='relu',input_dim=30))
+	model.add(Dense(50,activation='relu'))
+	model.add(Dropout(0.4))
+	model.add(Dense(1,activation='sigmoid'))
+	model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
 #Train the model
-if not os.path.exists('mymodel.h5'):
+	if not os.path.exists('mymodel.h5'):
 		model.fit(X_train,Y_train,epochs=100)
 		model.save('mymodel.h5')
-else:
-	model=load_model('mymodel.h5')
-	print(model.evaluate(X_test,Y_test)[1])
+	else:
+		model=load_model('mymodel.h5')
+		print(model.evaluate(X_test,Y_test)[1])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	machine=SupportVectorMachine()
+
+	machine.predict(X_test,Y_test)
